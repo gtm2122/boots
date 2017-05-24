ccf3ed58219ad3df42decf7fd06c63b68a59672a
gtm2122 <gtm2122@github.com>
2017-05-24 18:04:44 -0400

    some bug fixes

--- boots.py
+++ boots.py
@@ -1,5 +1,8 @@
 from xgboost import XGBClassifier
-class clf_boots(object):
+import numpy as np
+### Boosted classifiers of different sized sampled(with replacement) subsets of dataset
+
+class clf_boots2(object):
     def __init__(self,X,y,model=XGBClassifier,n=1000):
         self.model = model(nthread=-1)
         self.X = X
@@ -7,12 +10,23 @@ class clf_boots(object):
         self.n = n
         self.model_list = []
     
-    @staticmethod
-    def boots(n,sz):
+    def la(self):
+        nn = self.n
+        for i in range(0,self.n+20):
+            print(i)
+    
+    def this_boots(self):
         inds=[]
-        
-        for i in range(10,n):
-            ind = np.random.choice(sz,i)
+        #print('her')
+        #print(self.n)
+        for i in xrange(1,self.n):
+            #print(i)
+            #print('herr')
+            num = (i+10)%self.n
+            ind = np.random.choice(self.y.shape[0],num)
+            #print(np.random.choice(self.y.shape[0],i))
+            #print(ind)
+            #print(ind)
             inds+=[ind]
             #print(ind.max())
         return inds
@@ -22,9 +36,10 @@ class clf_boots(object):
         return self.model
     
     def train_models(self):
-        n = self.n
-        sz = self.y.shape[0]
-        boot_ind = self.boots(n,sz)
+        #print(self.n)
+        #print(self.y.shape[0])
+        boot_ind = self.this_boots()
+        #print(boot_ind)
         for i in boot_ind:
             #print(i)
             self.model_list.append(self.train(self.X[np.array(i),:],self.y[np.array(i)]))
@@ -32,19 +47,30 @@ class clf_boots(object):
         
     def forward(self,x):
         y = np.zeros((len(self.model_list),x.shape[0]))
+        #print(self.model_list)
         count = 0
         probs = np.zeros_like(y)
+        #print(probs)
         for i in self.model_list:
             y[count,:] = i.predict(x)
             #print(i.predict_proba(x))
             #print(i.predict_proba(x)[:,1])
             probs[count,:] = i.predict_proba(x)[:,1]
+            #print(probs[count,:])
             count+=1
-        return np.round(y.sum(axis=0).astype(float)/y.shape[0]),np.mean(probs,axis=0)
+        #print(probs)
+        prob_result = np.zeros((y.shape[1],2))
+        print np.mean(probs,axis=0)
+        prob_result[:,1] = np.mean(probs,axis=0)
+        prob_result[:,0] = 1 - prob_result[:,1]
+        
+        return np.round(y.sum(axis=0).astype(float)/y.shape[0]),prob_result
 #     def f_test(self,x):
 #         print(len(self.model_list))
 #         print(self.model_list[-1].predict(x))
     
     def test(self,x_test):
         ### Returns predictions and the prediction probabilities of positives
-        return self.forward(x_test)[0],self.forward(x_test)[1]
+        a,b = self.forward(x_test)
+        return a,b  
+ 
